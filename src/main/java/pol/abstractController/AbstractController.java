5ceb5bb190bdeef899b549f82651a61bc67def4b
Chalski <poczta@marcinmichalski.pl>
2014-03-29 15:04:33 +0100

    Modyfikacja AbstractController

--- src/main/java/pol/abstractController/AbstractController.java
+++ src/main/java/pol/abstractController/AbstractController.java
@@ -1,5 +1,22 @@
 package pol.abstractController;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 public abstract class AbstractController {
 
+	public static final String PAGE_TITLE = "pageTitle";
+	public static final String SHOW_MENU = "showMenu"; 
+	
+	protected static String pageTitle= "Przychodnia";
+	
+	@ModelAttribute(PAGE_TITLE)
+	protected String getPageTitle(){
+		return pageTitle;
+	}
+	
+	@ModelAttribute(SHOW_MENU)
+	protected boolean showMenu(){
+		return true;
+	}
+	
 }
