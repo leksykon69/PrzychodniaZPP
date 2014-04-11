26e5945af6f66af5c1c6492f9af905779a2357e8
Chalski <poczta@marcinmichalski.pl>
2014-04-11 10:10:28 +0200

    Rozwiązanie problemu z kodowaniem + Dodanie komunikatów

--- src/main/java/pol/abstractController/AbstractController.java
+++ src/main/java/pol/abstractController/AbstractController.java
@@ -1,15 +1,21 @@
 package pol.abstractController;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
+import org.joda.time.DateTime;
+import org.joda.time.LocalTime;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import pol.entity.MenuEntity;
+import pol.frontend.Message;
+import pol.frontend.MessageType;
+import pol.spring.bind.editors.DateTimeEditor;
+import pol.spring.bind.editors.LocalTimeEditor;
 
 @SessionAttributes(AbstractController.MENU)
 public abstract class AbstractController {
@@ -17,6 +23,7 @@ public abstract class AbstractController {
 	public static final String PAGE_TITLE = "pageTitle";
 	public static final String SHOW_MENU = "showMenu"; 
 	public static final String MENU = "menu";
+	public static final String MESSAGE = "message";
 	
 	protected static String pageTitle= "Przychodnia";
 	
@@ -29,6 +36,33 @@ public abstract class AbstractController {
 	protected boolean showMenu(){
 		return true;
 	}
+	
+	protected void addSuccessMessage(Model model, String message){
+		model.addAttribute(MESSAGE, new Message(message, MessageType.SUCCESS));
+	}
+	
+	protected void addInfoMessage(Model model, String message){
+		model.addAttribute(MESSAGE, new Message(message, MessageType.INFO));
+	}
+	
+	protected void addWarnMessage(Model model, String message){
+		model.addAttribute(MESSAGE, new Message(message, MessageType.WARN));
+	}
+	
+	protected void addErrorMessage(Model model, String message){
+		model.addAttribute(MESSAGE, new Message(message, MessageType.ERROR));
+	}
+	
+	protected void addErrorMessage(Model model, String message, Exception e){
+		model.addAttribute(MESSAGE, new Message(message, MessageType.ERROR, e));
+	}	   
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(DateTime.class, new DateTimeEditor());
+		binder.registerCustomEditor(LocalTime.class, new LocalTimeEditor());
+	}
+	
 	
 	
 	//FIXME Zmienić na dynamicznie wyciągane z bazy danych
