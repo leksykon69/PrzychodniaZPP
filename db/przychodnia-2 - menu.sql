65e81ebf6404f83b83a2c212ed49b262af109bce
Chalski <poczta@marcinmichalski.pl>
2014-05-03 10:19:34 +0200

    Menu i zmiana nazewnictwa patchy do bazy danych

---
+++ db/przychodnia-2 - menu.sql
@@ -0,0 +1,8 @@
+INSERT INTO `menu` (`id`, `title`, `url`) VALUES
+(1, 'Wizyty', '/Przychodnia/visit/'),
+(2, 'Lekarze', '/Przychodnia/doctors/'),
+(3, 'Pacjenci', '/Przychodnia/patients/'),
+(4, 'Pomieszczenia', '/Przychodnia/room/'),
+(5, 'Użytkownicy', '/Przychodnia/users/'),
+(6, 'Uprawnienia', '/Przychodnia/permissions/'),
+(7, 'Statystyki', '/Przychodnia/stats/');
